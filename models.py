641bff1856322bb540a6707d154d4f341e15654f
DangDang1895 <671117853@qq.com>
2024-01-01 12:39:12 +0800

    First Commit

--- models.py
+++ models.py
@@ -2,17 +2,102 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-class CNN(nn.Module):
-    def __init__(self,in_channels,out_dim):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, 6, kernel_size=5)
-        self.conv2 = nn.Conv2d(6, 10, kernel_size=5)
-        self.fc1 = nn.Linear(250, 128)
-        self.fc2 = nn.Linear(128, out_dim)
+# 定义模型
+class MobileNetV2(nn.Module):
+    def __init__(self, num_classes):
+        super(MobileNetV2, self).__init__()
 
-    def forward(self, X):
-        X = F.relu(F.max_pool2d(self.conv1(X), 2))
-        X = F.relu(F.max_pool2d(self.conv2(X), 2))
-        X = F.relu(self.fc1(X.reshape(-1,250)))
-        X = self.fc2(X)
-        return X
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1, groups=32, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 64, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(64),
+
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, groups=64, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 128, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(128),
+
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, groups=128, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(128),
+
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1, groups=128, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 256, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(256),
+
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, groups=256, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(256),
+
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, groups=256, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 512, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(512),
+
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, groups=512, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(512),
+
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, groups=512, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(512),
+
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1, groups=512, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 1024, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(1024),
+
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, groups=1024, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(1024, 1024, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(1024),
+
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d(1)
+        )
+
+        self.fc = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
